18c295b0d5a988857fc6d31a2964c605b7138f21
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2022-09-06 19:56:07 +0800

    fix build tooling

--- projects/example/src/app/app.module.ts
+++ projects/example/src/app/app.module.ts
@@ -3,9 +3,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgxDbModule } from 'ngx-db'
 import { NgxPushviewModule } from 'ngx-pushview'
-import { NgxAnimeModule } from 'projects/ngx-anime/src/anime.module'
 import { AppRoutingModule } from './app.routing.module'
-import { AnimeComponent } from './components/anime/anime.component'
 import { AppComponent } from './components/app/app.component'
 import { DbComponent } from './components/db/db.component'
 import { Publication } from './components/db/db.models'
@@ -16,7 +14,6 @@ import { PushviewComponent } from './components/pushview/pushview.component'
   declarations: [
     AppComponent,
     IndexComponent,
-    AnimeComponent,
     DbComponent,
     PushviewComponent
   ],
@@ -24,7 +21,6 @@ import { PushviewComponent } from './components/pushview/pushview.component'
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    NgxAnimeModule,
     NgxDbModule.forRoot({ name: 'example', version: 1, models: [Publication] }),
     NgxPushviewModule
   ],
